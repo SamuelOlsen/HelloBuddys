319a3b8a40ae3a4668367b1fac9436848df45db3
SamuelOlsen <108121587+SamuelOlsen@users.noreply.github.com>
2022-06-24 10:41:24 +0200

    Create Main.java
    just a simple while loop.

---
+++ Main.java
@@ -0,0 +1,18 @@
+package kap02;
+
+public class føsdag {
+
+	public static void main(String[] args) {
+	
+		int year = 15;
+		
+		while(year<18) {
+			System.out.println("you are not an adult");
+			alder++;
+			System.out.println("Congratz you are: "+alder);
+		}
+		System.out.println("Congrats you are an adult, and your age is: "+alder+" years");
+
+	}
+
+}
